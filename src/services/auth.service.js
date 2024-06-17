5f8b0593b9d060b358526b09e8769043774270aa
romesway <romesway@gmail.com>
2024-06-17 18:09:21 +0900

    feat/에러처리미들웨어, 에러클래스 구현 및 연동

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,4 +2,11 @@ export class AuthService {
     constructor(usersRepository) {
         this.usersRepository = usersRepository;
     }
+    // 회원가입 - service
+    signUpUser = async ({ email, password, role, city, name, phoneNumber }) => {
+        const condition = { email, password, role, city, name, phoneNumber };
+        console.log(condition.email);
+
+        const existUser = await this.usersRepository.findUser({ email });
+    };
 }
