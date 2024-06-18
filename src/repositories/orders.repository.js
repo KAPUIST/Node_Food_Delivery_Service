e0a5010f64140a8ebf5ed7d864539589dc58326d
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-06-18 14:01:08 +0900

    Feat/주문확인 기능 개발완료

--- src/repositories/orders.repository.js
+++ src/repositories/orders.repository.js
@@ -2,7 +2,44 @@ export default class OrdersRepository {
     constructor(prisma) {
         this.prisma = prisma;
     }
-
+    findOrderById = async (orderId) => {
+        return await this.prisma.orders.findUnique({
+            where: { id: orderId },
+            include: {
+                orderItems: true,
+            },
+        });
+    };
+    findOrdersByStatus = async (status, restaurantId) => {
+        return await this.prisma.orders.findMany({
+            where: { status, restaurantId },
+            include: {
+                orderItems: true,
+            },
+            orderBy: {
+                createdAt: 'desc',
+            },
+        });
+    };
+    isUserOwnerOfRestaurant = async (userId, restaurantId) => {
+        const restaurant = await this.prisma.restaurants.findUnique({
+            where: { id: restaurantId, ownerId: userId },
+        });
+        return restaurant;
+    };
+    // 최신순으로 최대 10개의 주문 조회
+    findRecentOrders = async (restaurantId) => {
+        return await this.prisma.orders.findMany({
+            where: { restaurantId },
+            take: 30,
+            include: {
+                orderItems: true,
+            },
+            orderBy: {
+                createdAt: 'desc',
+            },
+        });
+    };
     createOrderTx = async (customerId, restaurantId, orderItemsWithPrice, totalPrice, callback) => {
         //트랜잭션을 활용하여 주문을 생성합니다.
         return await this.prisma.$transaction(async (tx) => {
