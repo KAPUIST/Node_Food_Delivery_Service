1029355516fa7105eaab94cd7af879e4e55516d5
clearghost3 <165910897+clearghost3@users.noreply.github.com>
2024-06-18 20:42:01 +0900

    add:owner CRUD

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,11 +3,14 @@ import usersRouter from './users.router.js';
 import authRouter from './auth.router.js';
 import searchRouter from './search.router.js';
 import orderRouter from './order.router.js';
+import ownerRouter from './owner.router.js';
+
 const router = express.Router();
 
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
 router.use('/search', searchRouter);
 router.use('/order', orderRouter);
+router.use('/owner',ownerRouter);
 
 export default router;
